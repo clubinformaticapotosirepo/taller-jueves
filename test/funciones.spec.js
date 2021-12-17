b36fd05aabcac3d66d9eea860f1f1f76d8e2cd30
clubinformaticapotosirepo <aalvareza90@gmail.com>
2021-12-16 20:21:15 -0400

    las declaraciones de variables fueron modificadas

--- test/funciones.spec.js
+++ test/funciones.spec.js
@@ -1,26 +1,26 @@
 const {suma, dividir} = require("../src/funciones")
 
-describe("verifica que la funcion suma trabaje bien", ()=>{
+describe("Verifica que la funcion suma trabaje bien", ()=>{
     test("Debe devolver 9" , ()=>{
-        a = 4;
-        b = 5;
-        debeSer = 9;
+        const a = 4;
+        const b = 5;
+        const debeSer = 9;
         expect(suma(a, b)).toBe(debeSer)
     })
 } )
 
-describe("verifica que la funcion dividir trabaje bien", ()=>{
+describe("Verifica que la funcion dividir trabaje bien", ()=>{
     test("Debe devolver 3" , ()=>{
-        x = 9;
-        y = 3;
-        debeSer = 3;
+        const x = 9;
+        const y = 3;
+        const debeSer = 3;
         expect(dividir(x, y)).toBe(debeSer)
     })
 
-    test("Debe devolver 3" , ()=>{
-        x = 9;
-        y = 0;
-        debeSer = "no se puede dividir entre 0";
+    test('Debe devolver "no se puede dividir entre 0"' , ()=>{
+        const x = 9;
+        const y = 0;
+        const debeSer = "no se puede dividir entre 0";
         expect(dividir(x, y)).toBe(debeSer)
     })
 
